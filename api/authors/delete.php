4daf8518cfe92e83a06212e0cdfc70a97ed0587d
Jodi Garness <jlgarness@mail.fhsu.edu>
2023-03-20 14:39:55 -0700

    done

--- api/authors/delete.php
+++ api/authors/delete.php
@@ -6,31 +6,29 @@
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
     include_once '../../config/Database.php';
-    include_once '../../models/Post.php';
+    include_once '../../models/Author.php';
 
     // Instatiate DB and connect
     $database = new Database();
     $db = $database->connect();
 
-    // Instantiate blog post object 
-    $post = new Post($db);
+    // Instantiate author object 
+    $name = new Author($db);
 
     // Get the raw posted data
     $data = json_decode(file_get_contents("php://input"));
 
-    // Set id to be updated
-    $post->id = $data->id;
-
-    // Delete post
-    if($post->delete()){
-        echo json_encode(
-            array('message' => 'Post Deleted')
-        );
-    } else { 
-        echo json_encode(
-            array('message' => 'Post Not Deleted')
-        );
+    // Retrieve
+    if (!isset($data->id)){
+        // If no data
+        echo(json_encode(array('message' => 'Missing Required Parameters'))); 
+    } else {
+        $name->id = $data->id;
+        $name->delete();
+        echo(json_encode(array('id'=>$name->id)));
+        
     }
+
     
     
 
