5aa25bc71d68a84e925e5c126faab932c493a8c7
Jodi Garness <jlgarness@mail.fhsu.edu>
2023-03-21 10:56:45 -0700

    done

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -8,3 +8,53 @@
         header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
         exit();
     }
+     // If statement for type of request
+     if ($method === 'GET') {
+        try {
+            if (isset($_GET['id']) )
+            require_once 'read_single.php' ;
+           else
+            require_once 'read.php';
+
+        }
+        catch(ErrorException $e)
+        {
+            echo("Required file not found!");
+        
+        }
+     }
+     else if ($method === 'POST') {
+        try {
+            require_once 'create.php';
+
+        }
+        catch(ErrorException $e)
+        {
+            echo("Required file not found!");
+        
+        }
+     }
+     else if ($method === 'PUT') {
+        try {
+            require_once 'update.php';
+
+        }
+        catch(ErrorException $e)
+        {
+            echo("Required file not found!");
+        
+        }
+     }
+     else if ($method === 'DELETE') {
+        try {
+            require_once 'delete.php';
+
+        }
+        catch(ErrorException $e)
+        {
+            echo("Required file not found!");
+        
+        }
+     }
+     else
+        echo ("No function requested");
